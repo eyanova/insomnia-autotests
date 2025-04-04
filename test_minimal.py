259684346b7d71615e80ef2b1cb25a7f13c19de2
plasmik <plasmik@pc>
2025-04-04 16:35:17 +0300

    check chrome available via pytest

--- test_minimal.py
+++ test_minimal.py
@@ -1,28 +1,15 @@
+import os
 
 from check_internet import check_internet
-
-# from unittest import TestCase
-# class TryTesting(TestCase):
-#     def utest_always_passes(self):
-#         self.assertTrue(True)
-#     def utest_always_fails(self):
-#         self.assertTrue(False)
-
-
-# def test_always_passes4():
-#     assert True
-
-# def test_always_passes3():
-#     assert True
-
-def test_always_passes2():
-    assert True
 
 def test_always_passes():
     assert True
 
 def test_internet():
     assert check_internet()
+
+def test_chrome_available():
+    assert 0==os.system("type chrome")
 
 
 

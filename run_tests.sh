47f98e590a32b84448cf17e151246dc39637a6c5
plasmik <plasmik@pc>
2025-04-25 10:20:56 +0300

    added repeats, added short log

--- run_tests.sh
+++ run_tests.sh
@@ -1,12 +1,27 @@
+export NREPS=3
+
 echo "Gonna explicitly build docker compose for regress-tests"
 docker compose build
 
 echo "Gonna run regress-tests in docker compose"
 
 # docker compose run --remove-orphans easy_test &&
-docker compose run --remove-orphans tests &&
+docker compose run --remove-orphans tests |tee /tmp/tests.log
+# docker compose run  --remove-orphans easy_test |tee /tmp/tests.log
+exitcode=${PIPESTATUS[0]}
+
+rm -rf /tmp/tests.short.log
+cat /tmp/tests.log | sed -n "/ test session starts /,/^=========/p" | head -n -1 >> /tmp/tests.short.log
+cat /tmp/tests.log | sed -n "/ short test summary info /,/^=========/p" | head -n -1 >> /tmp/tests.short.log
+cat /tmp/tests.log | grep "^====.*passed in " >> /tmp/tests.short.log
+
+echo -e "\n:CLEAN OUTPUT:\n"
+cat /tmp/tests.short.log
+
+echo -e "\n:VERDICT:"
+[ $exitcode -eq 0 ] &&
     { echo "Tests passed!"; } ||
-    { echo "Tests failed!" ; exit 2; }
+    { echo "Tests failed!" ; (exit 2); }
 
 # docker compose down
 
